bece827147f5c97c36c7dd534478fba7bd6bfa15
Ekcode-lab <enockkalumba213@gmail.com>
2024-10-18 15:33:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,14 @@ with st.sidebar:
   # island,bill_length_mm,bill_depth_mm,flipper_length_mm,body_mass_g,sex
   island = st.selectbox('island',('Torgersen','Biscoe','Dream'))
   bill_length_mm = st.slider('bill length (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('bill depth (mm)', 13.1, 21.5, 17.1)
+  flipper_length_mm = st.slider('flipper length (mm)', 172.0, 231.0, 200.9)
+  body_mass_g = st.slider('body mass (g)', 2700.0, 6300.0, 4207.0)
+  sex = st.selectbox('Sex', ('MALE','FEMALE'))
+
+
+
+  
   
 
 
