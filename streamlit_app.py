0c5d5f4e0af7eaaa197cba81d062a2f32c894fa5
Ekcode-lab <enockkalumba213@gmail.com>
2024-10-18 17:33:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,13 @@ data = {'island': island,
         'sex': sex}
 input_df = pd.DataFrame(data, index=[0])
 input_penguins = pd.concat([input_df, X_raw],axis=0)
+
+with st.expander('input features'):
+  st.write('**input penguins**')
+  input_df
+  st.write('**input combined data**')
+  input_penguins
+  
  # encode x
 encode = ['island','sex']
 df_penguins = pd.get_dummies("input_penguins,prefix=encode")
@@ -50,17 +57,12 @@ target_mapper = {'Adelie':0,
 def target_encode(val):
   return target_mapper[val]
 y = y_raw.apply(target_encode)
-y
-y_raw
 
-with st.expander('input features'):
-  st.write('**input penguins**')
-  input_df
-  st.write('**input combined data**')
-  input_penguins
-  st.write('**Encoded features**')
+with st.expander('Data preparation')
+  st.write('**Encoded input penguins(X)**')
   input_row
-
+  st.write('**Encoded y**')
+  y
 
 
 
