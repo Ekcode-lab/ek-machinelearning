0a2e4bed5260680ff1896d8fea8f06f662f51d5b
Ekcode-lab <enockkalumba213@gmail.com>
2024-10-18 15:22:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ with st.expander('Clean Data'):
 with st.expander('Data Visualization'):
   st.scatter_chart(data=df, x="bill_length_mm", y="body_mass_g",color="species")
 
-with st.slidebar:
+with st.sidebar:
   st.header('input features')
   # island,bill_length_mm,bill_depth_mm,flipper_length_mm,body_mass_g,sex
   island = st.selectbox('island',('Torgersen','Biscoe','Dream'))
