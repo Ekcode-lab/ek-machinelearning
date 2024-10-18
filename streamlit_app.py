3de238d59d9121e88422f4ad397062b2f5685796
Ekcode-lab <enockkalumba213@gmail.com>
2024-10-18 09:53:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('🤖 WEATHER PREDICTION APP')
 
-st.write('This app is meant to predict weather conditions in your area 😊😊😊 ')
+st.write('This app is meant to predict weather conditions in your area')
