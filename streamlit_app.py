d6a369be27ec2d7325659bc2c0ca90f567465347
Ekcode-lab <enockkalumba213@gmail.com>
2024-10-18 16:11:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,10 @@ data = {'island': island,
         'flipper_length_mm': flipper_length_mm,
         'body_mass_g': body_mass_g,
         'sex': sex}
-input_df = pd.dataframe(data, index=[0])
-input_penguins = pd.concat([input_df],axis=0)
+input_df = pd.Dataframe(data, index=[0])
+input_penguins = pd.concat([input_df, X],axis=0)
+
+input_df
 
 
 
