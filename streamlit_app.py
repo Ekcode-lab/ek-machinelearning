8c16f9d404f161de0ccbade6cc1855794caa66d8
Ekcode-lab <enockkalumba213@gmail.com>
2024-10-18 14:58:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,9 @@ with st.expander('Clean Data'):
   st.write('**y**')
   y = df.species
   y
+with st.expander('Data Visualization')
+  st.scatter_chart(data=df, x="bill_length_mm", y="body_mass_g",color="species")
+
 
 
 
