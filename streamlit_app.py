b1108fef09608d66ac853846697b62ffd8d47862
Ekcode-lab <enockkalumba213@gmail.com>
2024-10-18 14:04:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,17 @@ st.title('🤖 WEATHER PREDICTION APP')
 
 st.write('This app is meant to predict weather conditions in your area')
 with st.expander('Data'):
-  st.write('Raw Data')
+  st.write('**Raw Data**')
   df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/refs/heads/master/penguins.csv')
   df
 with st.expander('Clean Data'):
-  st.write('clean data')
+  st.write('**clean data**')
   df.dropna(subset=['sex'],inplace=True)
   df
+  st.write('**X**')
+  X = df.drop('species',axis=1)
+  X
+
 
 
 
