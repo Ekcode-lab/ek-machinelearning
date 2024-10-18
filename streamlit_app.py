f145a7757761be74636587011d0f5add0403af5e
Ekcode-lab <enockkalumba213@gmail.com>
2024-10-18 10:13:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title('🤖 WEATHER PREDICTION APP')
 st.write('This app is meant to predict weather conditions in your area')
 
 df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/refs/heads/master/penguins.csv')
-df.head()
+df
 
 
 
